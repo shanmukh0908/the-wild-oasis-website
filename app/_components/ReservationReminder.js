e47018413fbb128beb3212a75de351bc87dca8b6
shanmukh <shanmukh0908@gmail.com>
2024-12-30 17:57:05 +0530

    modified5 commit

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -1,7 +1,7 @@
 "use client";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { format } from "date-fns";
-import { userReservation } from "./context";
+import { useReservation } from "./context";
 
 function ReservationReminder() {
   // CHANGE
